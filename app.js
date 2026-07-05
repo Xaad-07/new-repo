063b340d71e7fe9b82b9ea4d008093a5a90fad76
Xaad-07 <saadptl062@gmail.com>
2026-07-05 17:52:42 +0530

    chek out js changes

---
+++ app.js
@@ -0,0 +1,6 @@
+btn = document.querySelector('#myButton');
+btn.addEventListener('click', function() {
+  alert('Button clicked!');
+});
+
+/* thankyou soo much for your help! I really appreciate it. */
